86093b8a035a84566839a2f94fbd6570e0eded1f
chiru <nchiru987@gmail.com>
2023-05-19 08:25:29 +0530

    connect to db

--- src/components/players.js
+++ src/components/players.js
@@ -1,9 +1,24 @@
 import PlayerUi from "./playerui";
+import {useState,useEffect} from "react";
+
+
 const Players =() => {
+  const [players,setPlayers] = useState();
+  useEffect(()=>{
+      fetch('http://localhost:3005/players')
+      .then((res)=>res.json())
+      .then((data)=>setPlayers(data));
+  },[])
     return (
         <>
-          <PlayerUi name="Ms Dhoni" role="Right hand batshman"
-          matchsPlayed="13" runsScored="98"/>
+          <div className="flex flex-wrap justify-center">
+            {players ? players.map((player)=>{
+              return (
+                <PlayerUi name={player.name} role={player.role}
+                img={player.img} matchsPlayed={player.mat} runsScored={player.runs} team={player.team}/>
+              );
+            }):null}
+          </div>
         </>
     )
 }
